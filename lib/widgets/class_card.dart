63583e0fd65cd713f41dc4c551b5a6a7b4a1e2c7
Harsh Sharma <bcs_2019023@iiitm.ac.in>
2021-03-31 11:48:52 +0530

    added URL launcher for links

--- lib/widgets/class_card.dart
+++ lib/widgets/class_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:horario/providers/class.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../providers/class.dart';
 
 class ClassCard extends StatelessWidget {
   final Class c;
@@ -24,6 +25,20 @@ class ClassCard extends StatelessWidget {
         child: ListTile(
           title: Text(c.subject),
           subtitle: Text(timeString),
+          trailing: c.link != null
+              ? IconButton(
+                  icon: const Icon(Icons.videocam_rounded),
+                  onPressed: () async {
+                    if (await canLaunch(c.link ?? "")) {
+                      await launch(
+                        c.link ?? "",
+                        forceSafariVC: false,
+                      );
+                    } else {
+                      throw "Could not launch URL";
+                    }
+                  })
+              : null,
         ),
       ),
     );
